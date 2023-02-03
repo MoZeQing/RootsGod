7f48546e19f51960b2e51d9ebdef8fcc99dee4da
Drogon_7j <457684921@qq.com>
2023-02-03 20:02:38 +0800

    Create Urp Project And Import GameFramework

---
+++ RootsGod/Assets/GameMain/Scripts/Game/SurvivalGame.cs
@@ -0,0 +1,38 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework;
+using GameFramework.DataTable;
+using UnityEngine;
+
+namespace GameMain
+{
+    public class SurvivalGame : GameBase
+    {
+        private float m_ElapseSeconds = 0f;
+
+        public override GameMode GameMode
+        {
+            get
+            {
+                return GameMode.Survival;
+            }
+        }
+
+        public override void Update(float elapseSeconds, float realElapseSeconds)
+        {
+            base.Update(elapseSeconds, realElapseSeconds);
+
+            m_ElapseSeconds += elapseSeconds;
+            if (m_ElapseSeconds >= 1f)
+            {
+                m_ElapseSeconds = 0f;
+
+            }
+        }
+    }
+}
